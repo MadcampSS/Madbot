f1b6d974832c37f3374327281b0b8b544a6327c6
Seungwon Ju <dshseungwon@gmail.com>
2019-01-11 13:47:11 +0900

    Speech Recognition Added!

--- app/src/main/java/com/example/voicerecognition/MainActivity.java
+++ app/src/main/java/com/example/voicerecognition/MainActivity.java
@@ -10,6 +10,9 @@ import android.media.FaceDetector;
 import android.net.Uri;
 import android.os.Build;
 import android.provider.MediaStore;
+import android.speech.RecognitionListener;
+import android.speech.RecognizerIntent;
+import android.speech.SpeechRecognizer;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
@@ -22,7 +25,7 @@ import android.widget.Toast;
 
 import java.util.ArrayList;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements RecognitionListener {
 
     final int PERMISSION_READ_EXTERNAL_STORAGE = 111;
 
@@ -30,6 +33,11 @@ public class MainActivity extends AppCompatActivity {
     FaceDetectionActivity faceDetectionActivity;
 
     GridView gridView;
+
+    private SpeechRecognizer speech;
+
+    private Intent recognizerIntent;
+    private final int RESULT_SPEECH = 1000;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -40,6 +48,21 @@ public class MainActivity extends AppCompatActivity {
         faceDetectionActivity = new FaceDetectionActivity();
 
         checkPermission(this);
+
+        speech = SpeechRecognizer.createSpeechRecognizer(this);
+        speech.setRecognitionListener(this);
+
+        findViewById(R.id.floatingButton).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                recognizerIntent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
+                recognizerIntent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_PREFERENCE, "ko-KR"); //언어지정입니다.
+                recognizerIntent.putExtra(RecognizerIntent.EXTRA_CALLING_PACKAGE, getPackageName());
+                recognizerIntent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL, RecognizerIntent.LANGUAGE_MODEL_WEB_SEARCH);
+                recognizerIntent.putExtra(RecognizerIntent.EXTRA_MAX_RESULTS, 5);   //검색을 말한 결과를 보여주는 갯수
+                startActivityForResult(recognizerIntent, RESULT_SPEECH);
+            }
+        });
 
     }
 
@@ -141,4 +164,120 @@ public class MainActivity extends AppCompatActivity {
         return imageList;
     }
 
+    @Override
+    public void onEndOfSpeech() {
+
+    }
+
+    @Override
+    public void onReadyForSpeech(Bundle bundle) {
+
+    }
+
+    @Override
+    public void onResults(Bundle results) {
+        ArrayList<String> matches = results
+                .getStringArrayList(SpeechRecognizer.RESULTS_RECOGNITION);
+
+        for(int i = 0; i < matches.size() ; i++){
+            Log.e("GoogleActivity", "onResults text : " + matches.get(i));
+        }
+
+    }
+
+    @Override
+    public void onError(int errorCode) {
+
+        String message;
+
+        switch (errorCode) {
+
+            case SpeechRecognizer.ERROR_AUDIO:
+                message = "오디오 에러";
+                break;
+
+            case SpeechRecognizer.ERROR_CLIENT:
+                message = "클라이언트 에러";
+                break;
+
+            case SpeechRecognizer.ERROR_INSUFFICIENT_PERMISSIONS:
+                message = "퍼미션없음";
+                break;
+
+            case SpeechRecognizer.ERROR_NETWORK:
+                message = "네트워크 에러";
+                break;
+
+            case SpeechRecognizer.ERROR_NETWORK_TIMEOUT:
+                message = "네트웍 타임아웃";
+                break;
+
+            case SpeechRecognizer.ERROR_NO_MATCH:
+                message = "찾을수 없음";;
+                break;
+
+            case SpeechRecognizer.ERROR_RECOGNIZER_BUSY:
+                message = "바쁘대";
+                break;
+
+            case SpeechRecognizer.ERROR_SERVER:
+                message = "서버이상";;
+                break;
+
+            case SpeechRecognizer.ERROR_SPEECH_TIMEOUT:
+                message = "말하는 시간초과";
+                break;
+
+            default:
+                message = "알수없음";
+                break;
+        }
+
+        Log.e("GoogleActivity", "SPEECH ERROR : " + message);
+    }
+
+    @Override
+    public void onRmsChanged(float v) {
+
+    }
+
+    @Override
+    public void onBeginningOfSpeech() {
+
+    }
+
+    @Override
+    public void onEvent(int i, Bundle bundle) {
+
+    }
+
+    @Override
+    public void onPartialResults(Bundle bundle) {
+
+    }
+
+    @Override
+    public void onBufferReceived(byte[] bytes) {
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        switch (requestCode) {
+            case RESULT_SPEECH : {
+                if (resultCode == RESULT_OK && null != data) {
+                    ArrayList<String> text = data
+                            .getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS);
+
+                    for(int i = 0; i < text.size() ; i++){
+                        Log.e("GoogleActivity", "onActivityResult text : " + text.get(i));
+                    }
+                }
+
+                break;
+            }
+        }
+    }
 }
